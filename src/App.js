aff40657add6a1c3993124fe2d1df361b2629673
Cindy Tauane <cindy.tauane@idealtrends.local>
2024-06-08 21:35:57 -0300

    [BUILD]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/header.js';
 import Body from './components/body.js';
 import Footer from './components/footer.js';
+// eslint-disable-next-line
 import Style from './css/style.css';
 
 
